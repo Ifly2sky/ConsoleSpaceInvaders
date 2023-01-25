870d5a7ca56affadbca51fb97ee0b4cb7d573cf9
Ifly2sky <tinhau24@gmail.com>
2023-01-25 09:16:05 +0200

    Initial Commit

---
+++ Console_Space_Invaders/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Console_Space_Invaders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
